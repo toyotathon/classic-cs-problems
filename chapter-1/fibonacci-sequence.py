4ed1274887fadd4c42ad925473a2c5d4aca5b618
Jason Valenzuela <jason.e.valenzuela@gmail.com>
2020-05-03 20:54:32 -0700

    finishing fib

--- chapter-1/fibonacci-sequence.py
+++ chapter-1/fibonacci-sequence.py
@@ -10,8 +10,6 @@ def recursive_fib(n: int) -> int:
     return recursive_fib(n - 1) + recursive_fib(n - 2)
 
 # memoized example
-
-
 def memo_fib(n: int) -> int:
     if n not in memo:
         memo[n] = memo_fib(n - 1) + memo_fib(n - 2)  # memoization
@@ -25,8 +23,33 @@ def auto_memo_fib(n: int) -> int:
         return n
     return auto_memo_fib(n - 2) + auto_memo_fib(n - 1)  # recursive case
 
+# simple iterative approach
+def iterative_fib(n: int) -> int:
+    if n == 0:
+        return n  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+# creates a generator get fibonacci value
+def fib_generator(n: int) -> int:
+    yield 0  # special case
+    if n > 0:
+        yield 1  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next  # main generation step
+
 
 if __name__ == "__main__":
-    print("recursive", recursive_fib(5))
-    print("memoized", memo_fib(50))
-    print("auto memoized (LRU cache)", auto_memo_fib(50))
+    print("recursive\t", recursive_fib(5))
+    print("memoized\t", memo_fib(50))
+    print("auto memoized\t", auto_memo_fib(50))
+    print("iterative\t", iterative_fib(50))
+    print("\n\n")
+    for i in fib_generator(50):
+        print("generator value\t", i)
